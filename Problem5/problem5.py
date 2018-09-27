5c9df623a6c4c175d2a9530d3e6539dfa5fd923f
Kiteaholic <c.blasek@gmx.de>
2018-09-27 08:43:36 +0200

    test

--- Problem5/problem5.py
+++ Problem5/problem5.py
@@ -3,3 +3,25 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 #
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+def factorise(n):
+    l = []
+    while n % 2 == 0:
+        n = n // 2
+        l.append(2)
+    for i in range(3, n + 1, 2):
+        while n % i == 0:
+            n = n // i
+            l.append(i)
+        if i > n:
+            break
+    return l
+
+factors = []
+for i in range(2,20):
+   factors.append(factorise(i))
+# for fact in factors:
+
+
+print(factors)
+
--- functions.py
+++ functions.py
@@ -3,10 +3,26 @@ def factorise(n):
     while n % 2 == 0:
         n = n // 2
         l.append(2)
-    for i in range(3, n // 2, 2):
+    for i in range(3, n+1, 2):
         while n % i == 0:
             n = n // i
             l.append(i)
         if i > n:
             break
     return l
+
+
+def sieve_of_eratosthenes(n):
+    sieve = [True for _ in range(n - 1) ]
+    sieve[0] = [False]
+    for item in enumeratesieve:
+
+
+
+    
+    return sieve    
+
+
+
+
+print(sieve_of_eratosthenes(10))
